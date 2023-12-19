03ab9fe50e0afe9e5e99bd1847dc92509916c4e2
mgr-mirnal <mgr.mrinal97@gmail.com>
2023-12-19 14:57:54 +0000

    finished with funcstions

---
+++ function.js
@@ -0,0 +1,73 @@
+const readline = require('readline');
+
+const Rover = require('./rover');
+
+function runInstructions(topX, topY, roverPosition, instructions){
+    const[x, y , direction] = roverPosition.split(' ');
+    // console.log(x,y,direction)
+    // console.log(instructions)
+    // console.log(topX, topY)
+    const rover = new Rover(parseInt(x), parseInt(y), direction); // creating new instance of rover class
+
+    console.log(`Starting Positon: ${rover.getPosition()}`)
+    try{
+        console.log("ok")
+        for (const instruction of instructions){
+            if(instruction === "L"){
+                rover.turnLeft();
+            }else if(instruction === "R"){
+                rover.turnRight()
+            }else if(instruction === "M"){
+                rover.moveForward(topX, topY)
+            }
+        }
+    }catch(error){
+        console.error("Error inputing max coordinates: ", error);
+    }
+    console.log(`Ending Position: ${rover.getPosition()}`);
+    return rover.getPosition();
+}
+
+const r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let roverIndex = 0;
+const roverPositions = [];
+let maxCoordinates;
+
+function processRover(){
+    r.question(`Enter rover ${roverIndex + 1} position: `, (roverPosition) => {
+        r.question(`Enter the rover ${roverIndex +1} instruction: `, (givenInstruction) => {
+            try {
+                const endingPostion = runInstructions(maxCoordinates[0], maxCoordinates[1], roverPosition, givenInstruction);
+                roverPositions.push(endingPostion);
+                
+                roverIndex++;
+                if(roverIndex < 2){
+                    processRover();
+                }else{
+                    console.log('Ending Positions: ');
+                    for(let i = 0; i < roverPositions.length; i++){
+                        console.log(` Rover ${i + 1}: ${roverPositions[i]}`);
+                    }
+                    r.close()
+                }
+            }catch(error){
+                console.error('Error processing rover:', error);
+                r.close();
+            }
+        })
+    })
+}
+
+
+r.question('Enter Plature max coordinates: ', (givenCoordinates) => {
+    try{
+        maxCoordinates = givenCoordinates.split(' ').map(Number);
+        processRover();
+    }catch(error){
+        console.error("Error inputing max coordinates: ", error);
+    }
+})
--- rover.js
+++ rover.js
@@ -6,26 +6,42 @@ class Rover{
     }
 
     turnLeft(){
+        // console.log(this.direction)
             if(this.direction === 'N'){
-                this.direction == 'W'
-            }else if(this.direction === '"W"'){
-                this.direction == 'S'
+                this.direction = 'W'
+            }else if(this.direction === 'W'){
+                this.direction = 'S'
             }else if(this.direction === 'S'){
-                this.direction == 'E'
+                this.direction = 'E'
             }else if(this.direction === 'E'){
-                this.direction == 'N'
+                this.direction = 'N'
             }
     }
     turnRight(){
         if(this.direction === 'N'){
-            this.direction == 'E'
+            this.direction = 'E'
         }else if(this.direction === '"E"'){
-            this.direction == 'S'
+            this.direction = 'S'
         }else if(this.direction === 'S'){
-            this.direction == 'W'
+            this.direction = 'W'
         }else if(this.direction === 'W'){
-            this.direction == 'N'
+            this.direction = 'N'
         }
     }
-    
+    moveForward(topX, topY) {
+        if (this.direction === 'N') {
+            if (this.y < topX) {this.y++};
+        } else if (this.direction === 'E') {
+            if (this.x < topY) { this.x++};
+        } else if (this.direction === 'S') {
+            if (this.y > 0) {this.y--};
+        } else if (this.direction === 'W') {
+            if (this.x > 0) {this.x--};
+        }
+    }
+    getPosition(){
+        return `${this.x} ${this.y} ${this.direction}` // funciton to retrun rover postions
+    }
 }
+
+module.exports = Rover;
